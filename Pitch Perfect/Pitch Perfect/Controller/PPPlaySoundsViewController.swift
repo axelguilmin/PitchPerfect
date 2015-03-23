dd472e7b17e2cb0db5853b9a42146a8039dcc7b2
Axel Guilmin <github@axelguilmin.fr>
2015-03-24 00:16:57 +0100

    Playing audio

--- Pitch Perfect/Pitch Perfect/Controller/PPPlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/Controller/PPPlaySoundsViewController.swift
@@ -7,18 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PPPlaySoundsViewController: UIViewController {
 
+    var _audioPlayer:AVAudioPlayer!
+    
+    // MARK: - LAYOUT
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let fileURL:NSURL? = NSBundle.mainBundle().URLForResource("movie_quote", withExtension:"mp3") {
+            _audioPlayer = AVAudioPlayer(contentsOfURL:fileURL, error: nil)
+            _audioPlayer.enableRate = true
+        }
+        else {
+            println("the fileURL is empty, can't init _audioPlayer");
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - ACTION
 
+    @IBAction func playSlow() {
+        _audioPlayer.rate = 0.5
+        _audioPlayer.play()
+    }
+    @IBAction func playFast() {
+        _audioPlayer.rate = 2.0
+        _audioPlayer.play()
+    }
+    
+    @IBAction func stop() {
+        _audioPlayer.stop();
+    }
+    
 }
